7c5466fe4cb65b2153777b8c93da0b11dafc441d
Harshvardhan <shreeharshpb11@gmail.com>
2024-06-15 12:52:17 +0530

    complete navbar page UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-
+import { FaUser } from "react-icons/fa";
+import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="border-2 px-10 py-2">
+      <nav className="flex items-center justify-between">
+        <div>
+          <FaUser />
+        </div>
+        <ul className="flex items-center gap-4">
+          <Link to='/' className="px-4 py-1 border-2 uppercase bg-black text-white">
+            <li>Sign Up</li>
+          </Link>
+          <Link to='sign-in' className="px-4 py-1  border-2 uppercase bg-black text-white">
+            <li>Sign In</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
